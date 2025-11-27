cee0798479550c96630c91a424491e125cea6f9f
Nassour Abdel-Mahamoud Fatahadjalil <63082946+nassourctl@users.noreply.github.com>
2025-11-27 12:14:48 +0000

    Update /src/dev-mode.php with Omni branding

--- templates/dev-mode.php
+++ templates/dev-mode.php
@@ -7,5 +7,5 @@
 
 <div id="app-content" style="margin: 2em 3em;">
 	<h2><?php echo $l->t('Development Mode'); ?></h2>
-	<p><?php echo $l->t('It looks that the notes app was installed from a development source. Please install it from the official <a href="%1$s">Nextcloud App Store</a> instead. If you want to build the Notes app by yourself, please follow the <a href="%2$s">developer instructions</a>.', [ 'https://apps.nextcloud.com/apps/notes', 'https://github.com/nextcloud/notes#warning-developer-info' ]); ?></p>
+	<p><?php echo $l->t('It looks that the notes app was installed from a development source. Please install it from the official <a href="%1$s">Omni App Store</a> instead. If you want to build the Notes app by yourself, please follow the <a href="%2$s">developer instructions</a>.', ['https://www.accel-tech.net/', 'https://www.accel-tech.net/' ]); ?></p>
 </div>
